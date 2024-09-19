b33a4d52dab7f461b62f69af1d519f2f98ec8eef
bongj9 <lollypark501@naver.com>
2024-09-19 14:50:05 +0900

    initital commit

---
+++ src/main/java/jeonsaeyukjun/jeonsaeyukjun_be/JeonsaeyukjunBackendApplication.java
@@ -0,0 +1,13 @@
+package jeonsaeyukjun.jeonsaeyukjun_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonsaeyukjunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonsaeyukjunBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jeonsaeyukjun/jeonsaeyukjun_be/JeonsaeyukjunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jeonsaeyukjun.jeonsaeyukjun_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonsaeyukjunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
